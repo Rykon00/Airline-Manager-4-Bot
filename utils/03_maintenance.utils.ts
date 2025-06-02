c06708f89d516b94b3c98dcff2ca89af26974737
Til Meinel <98281093+Rykon00@users.noreply.github.com>
2025-06-02 16:56:13 +0200

    added Maintenance log messages

--- utils/03_maintenance.utils.ts
+++ utils/03_maintenance.utils.ts
@@ -9,26 +9,37 @@ export class MaintenanceUtils {
     }
 
     public async repairPlanes() {
+        console.log('Starting repairPlanes method...');
         await this.page.getByRole('button', { name: ' Plan' }).click();
+        console.log('Clicked on Plan button.');
         await this.page.getByRole('button', { name: ' Bulk repair' }).click();
+        console.log('Clicked on Bulk repair button.');
         await this.page.locator('#repairPct').selectOption('60');
+        console.log('Selected repair percentage to 60%.');
         await GeneralUtils.sleep(1000);
         const noPlaneExists = await this.page.getByText('There are no aircraft worn to').isVisible();
         if(!noPlaneExists) {
+            console.log('Planes found for repair. Proceeding with bulk repair.');
             await this.page.getByRole('button', { name: 'Plan bulk repair' }).click();
+        } else {
+            console.log('No planes require repair.');
         }
+        console.log('Completed repairPlanes method.');
     }
 
     public async checkPlanes() {
+        console.log('Starting checkPlanes method...');
         await this.page.getByRole('button', { name: ' Plan' }).click();
+        console.log('Clicked on Plan button.');
         await this.page.getByRole('button', { name: ' Bulk check' }).click();
+        console.log('Clicked on Bulk check button.');
 
         await GeneralUtils.sleep(2000);
         let clicked = false;
 
-        // Click only planes with danger text
         const dangerChecksExits = await this.page.locator('.bg-white > .text-danger').first().isVisible();
         if(dangerChecksExits) {
+            console.log('Danger checks exist. Clicking on planes with danger text.');
             const allCheckHoursDanger = await this.page.locator('.bg-white > .text-danger');
             let count = await allCheckHoursDanger.count();        
             for(let i = 0; i < count; i++) {
@@ -37,12 +48,17 @@ export class MaintenanceUtils {
                 await element.click();
                 clicked = true;
 
+                console.log(`Clicked on danger check element ${i + 1} of ${count}.`);
                 await GeneralUtils.sleep(500);
             }
+        } else {
+            console.log('No danger checks found.');
         }
 
         if(clicked) {
+            console.log('Planning bulk check for clicked planes.');
             await this.page.getByRole('button', { name: 'Plan bulk check' }).click();
         }
+        console.log('Completed checkPlanes method.');
     }
 }
